9a9213a5fd497d97b77f2fa0d73cb44e979b230a
Harsh <73747616+Harsh-pgmr@users.noreply.github.com>
2020-11-09 14:10:50 +0530

    updated Change_Mac

--- mac_Changer.py
+++ mac_Changer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
+#import subprocess and optparse
 import subprocess
 import optparse
 
 
-def get_help():
+def get_help():     #FUNCTION FOR CREATING PARSE OBJECTS 
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="iface", help="Interface to Change its Mac Address")
     parser.add_option("-m", "--mac", dest="new_mac", help="Enter new Mac Address you like")
@@ -15,12 +16,12 @@ def get_help():
     return options
 
 
-def change_mac(iface, new_mac):
+def change_mac(iface, new_mac):     #FUNCTION FOR SYSTEM COMMANDS
     print(" [+]Mac Address for " + iface + " is changed to " + new_mac)
     subprocess.call(["ifconfig", iface, "down"])
     subprocess.call(["ifconfig", iface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", iface, "up"])
 
-
+#CALIING change_mac AND get_help
 options = get_help()
 change_mac(options.iface, options.new_mac)
