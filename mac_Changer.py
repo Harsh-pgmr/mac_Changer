b79636b1b5e170c2a15719b2b79a77033513a0c9
Harsh-pgmr <73747616+Harsh-pgmr@users.noreply.github.com>
2020-11-09 13:16:45 +0530

    Change MAC Address
    Used Python

---
+++ mac_Changer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import subprocess
+import optparse
+
+
+def get_help():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="iface", help="Interface to Change its Mac Address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="Enter new Mac Address you like")
+    (options, arguments) = parser.parse_args()
+    if not options.iface:
+        parser.error("Please Enter interface or type --help")
+    elif not options.new_mac:
+        parser.error("Please Enter New Mac Address or type --help")
+    return options
+
+
+def change_mac(iface, new_mac):
+    print(" [+]Mac Address for " + iface + " is changed to " + new_mac)
+    subprocess.call(["ifconfig", iface, "down"])
+    subprocess.call(["ifconfig", iface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", iface, "up"])
+
+
+options = get_help()
+change_mac(options.iface, options.new_mac)
